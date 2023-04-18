f2ffe4df57edfa494bab6289deeb3c0dbab5bd50
Dasha <99654951+phvrsyde@users.noreply.github.com>
2023-04-18 12:11:37 +0300

    Update Main.java

--- src/main/java/com/dashad/Main.java
+++ src/main/java/com/dashad/Main.java
@@ -2,6 +2,6 @@ package com.dashad;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru!");
+        System.out.println("Hello qa.guru!!!!!!!");
     }
 }
